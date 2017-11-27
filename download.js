c5a59c1d1567729f8c7d49fc2d3a84aaa24daf30
Hidenori Fujimura <hfu@users.noreply.github.com>
2017-11-26 19:32:57 -0500

    use npm and docker-compose

--- download.js
+++ download.js
@@ -1,13 +1,24 @@
-const {exec} = require('child_process')
+const wget = require('node-wget')
+const fs = require('fs')
 const target = [
   ['50m', 'physical', 'land'],
   ['10m', 'cultural', 'populated_places']
 ]
 target.forEach(r => {
   [scale, category, theme] = r
-  url = `http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/${scale}/${category}/ne_${scale}_${theme}.zip`
-  exec(`wget -c -O src/${theme}.zip ${url}`, (err, stdout, stderr) => {
-    if(err) console.error(err.stack)
-    console.error(stderr)
+  arg = {
+    url: `http://www.naturalearthdata.com/http//www.naturalearthdata.com/` +
+      `download/${scale}/${category}/ne_${scale}_${theme}.zip`,
+    dest: `src/${theme}.zip`
+  }
+  fs.open(arg.dest, 'r', (err, fd) => {
+    if(err) {
+      wget(arg, (err, resp, body) => {
+        if(err) console.error(err.stack)
+      })
+    } else {
+      console.log(`skip ${arg.dest} because it exists`)
+    }
   })
+
 })
